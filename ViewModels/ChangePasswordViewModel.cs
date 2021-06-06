69523af810e22cf02d1eac55c35faf8c6707059e
Валерьян Аббазов <asr77734@gmail.com>
2021-06-06 16:17:40 +0500

    обновлён дизайн

--- ViewModels/ChangePasswordViewModel.cs
+++ ViewModels/ChangePasswordViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace ds3Wiki.ViewModels
         {
             public string Id { get; set; }
             public string Email { get; set; }
+            [Required]
             public string NewPassword { get; set; }
         }
 }
--- ViewModels/EditUserViewModel.cs
+++ ViewModels/EditUserViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace ds3Wiki.ViewModels
     public class EditUserViewModel
     {
         public string Id { get; set; }
+        [Required]
         public string Email { get; set; }
     }
 }
